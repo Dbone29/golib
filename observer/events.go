c29ce2d6c132463a6798f83e94133dbacb072831
Daniel <info@daniel-b.de>
2025-02-24 13:24:43 +0100

    Update events.go

--- observer/events.go
+++ observer/events.go
@@ -5,7 +5,7 @@ type Listener[T any] func(T)
 type Manager[T any] interface {
 	Add(n string, l Listener[T])
 	//Remove(n string, l Listener[T])
-	Invoke(n string, args any)
+	Invoke(n string, args Event[T])
 }
 
 type BaseManager[T any] struct {
